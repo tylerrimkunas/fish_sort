c9c7ab03cb3e586e73fedb1d0c0f8891ca1850b5
Tyler <tylerrimkunas@gmail.com>
2024-07-23 14:39:30 -0400

    Changed documentation

--- validate.hpp
+++ validate.hpp
@@ -8,7 +8,7 @@ validate_csv confirms that the csv file exists and that it is a csv file and not
 
 validate_source_dir confirms the existence of the source directory
 
-validate_destination_dir confirms that the directory exists, and if not then confirms that the string is in an appropriate format to create a directory and follows all constraints
+validate_destination_dir confirms that the directory exists, and if not then attempts to create directory
 */
 bool validate_csv(string s);
 bool validate_source_dir(string s);
